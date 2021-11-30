daa3ce1db44ae839ccbb515cf029dda10cb9d48e
Vladimir <nvn-65@mail.ru>
2021-11-30 22:05:07 +0300

    Запуск через Yandex1

--- functional_tests.py
+++ functional_tests.py
@@ -1,13 +1,39 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 # browser = webdriver.Firefox()
 # browser.get('http://localhost:8000')
 
+ser = Service('d:\\yandexdriver.exe')
 options = webdriver.ChromeOptions()
-binary_yandex_driver_file = 'd:\\yandexdriver.exe'  # path to YandexDriver
-driver = webdriver.Chrome(binary_yandex_driver_file)
-# driver = webdriver.Chrome(binary_yandex_driver_file, options=options)
+driver = webdriver.Chrome(service=ser, options=options)
+
+
+# Эдит слышала про крутое новое онлайн-приложение со списком
+# неотложных дел. Она решает оценить его домашнюю страницу
 driver.get('http://localhost:8000')
 
-assert 'Установка прошла успешно! Поздравляем!' in driver.title
+# Она видит, что заголовок и шапка страницы говорят о списках
+# неотложных дел
+# assert 'To-Do' in driver.title
+
+# Ей сразу же предлагается ввести элемент списка
+
+# Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби –
+# вязание рыболовных мушек)
+
+# Когда она нажимает enter, страница обновляется, и теперь страница
+# содержит "1: Купить павлиньи перья" в качестве элемента списка
+
+# Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
+# Она вводит "Сделать мушку из павлиньих перьев"
+# (Эдит очень методична)
+
+# Страница снова обновляется, и теперь показывает оба элемента ее списка
+# Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
+# сайт сгенерировал для нее уникальный URL-адрес – об этом
+# выводится небольшой текст с объяснениями.
+
+# Она посещает этот URL-адрес – ее список по-прежнему там.
+# Удовлетворенная, она снова ложится спать
 # driver.quit()
