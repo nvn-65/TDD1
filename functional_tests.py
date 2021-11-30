c3c5aa411c96243415b107bdffb807b32a8a00f7
Vladimir <nvn-65@mail.ru>
2021-11-30 21:35:25 +0300

    Запуск через Yandex

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,13 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+# browser = webdriver.Firefox()
+# browser.get('http://localhost:8000')
 
-assert 'Установка прошла успешно! Поздравляем!' in browser.title
+options = webdriver.ChromeOptions()
+binary_yandex_driver_file = 'd:\\yandexdriver.exe'  # path to YandexDriver
+driver = webdriver.Chrome(binary_yandex_driver_file)
+# driver = webdriver.Chrome(binary_yandex_driver_file, options=options)
+driver.get('http://localhost:8000')
+
+assert 'Установка прошла успешно! Поздравляем!' in driver.title
+# driver.quit()
