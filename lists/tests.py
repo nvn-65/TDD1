8b103def992e4c99fd4b23df709ee331bfe6c4ef
Vladimir <nvn-65@mail.ru>
2021-12-05 16:24:04 +0300

    стр.62 Первый модульный тест

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,12 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 
 class SmokeTest(TestCase):
     """тест на токсичность"""
 
-    def test_bad_maths(self):
-        """тест: неправельные математические расчёты"""
-        self.assertEqual(1+1, 3)
-
+    def test_root_url_resolves_to_home_page_view(self):
+        """тест: корневой url преобразуется в представление домашней страницы"""
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
