1ba7f623056a7f15c3d9faec0df1ca916a6706fd
Vladimir <nvn-65@mail.ru>
2021-12-11 17:23:23 +0300

    стр.74 ФТ теперь проверяет возможность ввести элемент списка

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,11 @@
+import time
+
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service
 import unittest
+
+from selenium.webdriver.common.by import By
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -29,21 +34,27 @@ class NewVisitorTest(unittest.TestCase):
         # Она видит, что заголовок и шапка страницы говорят о списках
         # неотложных дел
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест')  # Напоминание об окончании теста
-
+        header_text = self.browser.find_element(By.TAG_NAME('h1')).text
+        self.assertIn('To-Do', header_text)
 
 # Ей сразу же предлагается ввести элемент списка
-
+        inputbox = self.browser.find_element(By.ID('id_new_item'))
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Введите задачу')
 # Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби –
 # вязание рыболовных мушек)
-
+        inputbox.send_keys('Купить павлиньи перья')
 # Когда она нажимает enter, страница обновляется, и теперь страница
 # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element(By.ID('id_list_table'))
+        rows = table.find_elements(By.TAG_NAME('tr'))
+        self.assertTrue(any(row.text == '1: Купить павлиньи перья' for row in rows))
 # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
 # Она вводит "Сделать мушку из павлиньих перьев"
 # (Эдит очень методична)
-
+        self.fail('Закончить тест')  # Напоминание об окончании теста
 # Страница снова обновляется, и теперь показывает оба элемента ее списка
 # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
 # сайт сгенерировал для нее уникальный URL-адрес – об этом
