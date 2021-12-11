de2e27ada58fd2e61a232c562d1bdb8be0bb479e
Vladimir <nvn-65@mail.ru>
2021-12-11 19:23:01 +0300

    стр.84 HTML главной страницы теперь генерируется из шаблона

--- functional_tests.py
+++ functional_tests.py
@@ -23,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self) -> None:
         """завершение"""
-        self.browser.quit()
+        # self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """тест.можно начать список и получить его позже"""
@@ -33,12 +33,12 @@ class NewVisitorTest(unittest.TestCase):
 
         # Она видит, что заголовок и шапка страницы говорят о списках
         # неотложных дел
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME('h1')).text
-        self.assertIn('To-Do', header_text)
+        self.assertIn('To-Do lists', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('Your To-Do list', header_text)
 
 # Ей сразу же предлагается ввести элемент списка
-        inputbox = self.browser.find_element(By.ID('id_new_item'))
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Введите задачу')
 # Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби –
 # вязание рыболовных мушек)
@@ -48,9 +48,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID('id_list_table'))
-        rows = table.find_elements(By.TAG_NAME('tr'))
-        self.assertTrue(any(row.text == '1: Купить павлиньи перья' for row in rows))
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(any(row.text == '1: Купить павлиньи перья' for row in rows),
+                        "Новый элемент списка не появился в таблице")
 # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
 # Она вводит "Сделать мушку из павлиньих перьев"
 # (Эдит очень методична)
