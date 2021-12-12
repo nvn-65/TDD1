08ea3464357656bb9cb6cdbc5a1d7b691bd15c35
Vladimir <nvn-65@mail.ru>
2021-12-12 15:31:23 +0300

    стр.98

--- functional_tests.py
+++ functional_tests.py
@@ -1,19 +1,13 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service
 import unittest
-
 from selenium.webdriver.common.by import By
 
 
 class NewVisitorTest(unittest.TestCase):
     """тест нового посетителя"""
-
-    def setUp(self) -> None:
-        """установка"""
-
     # browser = webdriver.Firefox()
     # browser.get('http://localhost:8000')
 
@@ -21,18 +15,21 @@ class NewVisitorTest(unittest.TestCase):
     options = webdriver.ChromeOptions()
     browser = webdriver.Chrome(service=ser, options=options)
 
+    def setUp(self) -> None:
+        """установка"""
+
     def tearDown(self) -> None:
         """завершение"""
         # self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """тест.можно начать список и получить его позже"""
-        # Эдит слышала про крутое новое онлайн-приложение со списком
-        # неотложных дел. Она решает оценить его домашнюю страницу
+# Эдит слышала про крутое новое онлайн-приложение со списком
+# неотложных дел. Она решает оценить его домашнюю страницу
         self.browser.get('http://localhost:8000')
 
-        # Она видит, что заголовок и шапка страницы говорят о списках
-        # неотложных дел
+# Она видит, что заголовок и шапка страницы говорят о списках
+# неотложных дел
         self.assertIn('To-Do lists', self.browser.title)
         header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('Your To-Do list', header_text)
@@ -50,16 +47,25 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(any(row.text == '1: Купить павлиньи перья' for row in rows),
-                        "Новый элемент списка не появился в таблице")
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+
 # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
-# Она вводит "Сделать мушку из павлиньих перьев"
-# (Эдит очень методична)
-        self.fail('Закончить тест')  # Напоминание об окончании теста
+# Она вводит "Сделать мушку из павлиньих перьев" (Эдит очень методична)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
 # Страница снова обновляется, и теперь показывает оба элемента ее списка
-# Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.ID, 'tr')
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
+
+        # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
 # сайт сгенерировал для нее уникальный URL-адрес – об этом
 # выводится небольшой текст с объяснениями.
+        self.fail('Закончить тест')  # Напоминание об окончании теста
 
 # Она посещает этот URL-адрес – ее список по-прежнему там.
 # Удовлетворенная, она снова ложится спать
--- lists/tests.py
+++ lists/tests.py
@@ -8,3 +8,15 @@ class HomePageTest(TestCase):
         """тест: домашняя страница возвращает правильный html"""
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_uses_home_template(self):
+        """тест: используется домашний шаблон"""
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        """тест: можно сохранить post-запрос"""
+        response = self.client.post('/', data={'item_text': 'Новый элемент списка'})
+        self.assertIn('Новый элемент списка', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
