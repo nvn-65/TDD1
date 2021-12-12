6613367a16b57da700e438e2951584585a38aeca
Vladimir <nvn-65@mail.ru>
2021-12-12 15:42:09 +0300

    стр.100

--- functional_tests.py
+++ functional_tests.py
@@ -22,6 +22,12 @@ class NewVisitorTest(unittest.TestCase):
         """завершение"""
         # self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """подтверждение строки в таблице списка"""
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """тест.можно начать список и получить его позже"""
 # Эдит слышала про крутое новое онлайн-приложение со списком
@@ -44,6 +50,7 @@ class NewVisitorTest(unittest.TestCase):
 # содержит "1: Купить павлиньи перья" в качестве элемента списка
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
@@ -57,10 +64,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
 # Страница снова обновляется, и теперь показывает оба элемента ее списка
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.ID, 'tr')
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
-        self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
 # сайт сгенерировал для нее уникальный URL-адрес – об этом
