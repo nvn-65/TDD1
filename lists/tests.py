bc33496450ca8a7b07803ebf702712ee8a161f77
Vladimir <nvn-65@mail.ru>
2021-12-19 17:49:15 +0300

    стр.115 Переадресация после POST и показ всех элементов в шаблоне

--- lists/tests.py
+++ lists/tests.py
@@ -27,6 +27,11 @@ class ItemModelTest(TestCase):
 class HomePageTest(TestCase):
     """тест домашней страницы"""
 
+    def test_only_saves_items_when_necessary(self):
+        """тест: сохраняет элементы, только когда нужно"""
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_home_page_returns_correct_html(self):
         """тест: домашняя страница возвращает правильный html"""
         response = self.client.get('/')
@@ -39,6 +44,24 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         """тест: можно сохранить post-запрос"""
+        self.client.post('/', data={'item_text': 'Новый элемент списка'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'Новый элемент списка')
+
+    def test_redirects_after_POST(self):
+        """тест: переадресует после post-запроса"""
         response = self.client.post('/', data={'item_text': 'Новый элемент списка'})
-        self.assertIn('Новый элемент списка', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def tes_t_displays_all_list_items(self):
+        """тест: отображаются все элементы списка"""
+        Item.objects.create(text='ite_mey 1')
+        Item.objects.create(text='ite_mey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('ite_mey 1', response.content.decode())
+        self.assertIn('ite_mey 2', response.content.decode())
