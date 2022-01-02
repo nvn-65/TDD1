032384c1abdb28e762b0ab624be232509eab5344
Vladimir <nvn-65@mail.ru>
2022-01-02 16:31:42 +0300

    стр.137

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,12 @@
 import time
+from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from django.test import LiveServerTestCase
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -22,14 +25,22 @@ class NewVisitorTest(LiveServerTestCase):
         """завершение"""
         # self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        """подтверждение строки в таблице списка"""
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        """ожидать строку в таблице списка"""
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        """тест.можно начать список и получить его позже"""
+    def test_can_start_a_list_for_one_user(self):
+        """тест.можно начать список для одного пользователя"""
 # Эдит слышала про крутое новое онлайн-приложение со списком
 # неотложных дел. Она решает оценить его домашнюю страницу
         self.browser.get(self.live_server_url)
@@ -49,8 +60,7 @@ class NewVisitorTest(LiveServerTestCase):
 # Когда она нажимает enter, страница обновляется, и теперь страница
 # содержит "1: Купить павлиньи перья" в качестве элемента списка
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
@@ -61,16 +71,61 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Сделать мушку из павлиньих перьев')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
 # Страница снова обновляется, и теперь показывает оба элемента ее списка
-        self.check_for_row_in_list_table('1: Купить павлиньи перья')
-        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
+        self.wait_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
 
-        # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
+
+# Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
 # сайт сгенерировал для нее уникальный URL-адрес – об этом
 # выводится небольшой текст с объяснениями.
         self.fail('Закончить тест')  # Напоминание об окончании теста
 
 # Она посещает этот URL-адрес – ее список по-прежнему там.
 # Удовлетворенная, она снова ложится спать
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        """тест: многочисленные пользователи могут начать списки по разным url"""
+        # Эдит начинает новый список
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+
+        # Она замечает, что её список имеет уникальный URL-адрес
+        edit_list_url = self.browser.current_url
+        self.assertRegex(edit_list_url, 'lists/.+')
+
+        # Теперь новый пользователь, Френсис, приходит на сайт.
+
+        ## Мы используем новый сеанс браузера, тем самым обеспечивая, чтобы никокая
+        ## информация от Эдит не прошла через данные cookie и пр.
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        # Френсис посещает домашнюю страницу. Нет никаких признаков списка Эдит.
+        self.browser.get(self.live_server_url)
+        page_test = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Купить павлиньи перья', page_test)
+        self.assertNotIn('Сделать мушку', page_test)
+
+        # Фрэнсис начинает новый список, вводя новый элемент. Он менее
+        # интересен, чем список Эдит...
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Купить молоко')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молоко')
+
+        # Френсис получает уникальный URL-адрес
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertEqual(francis_list_url, edit_list_url)
+
+        # Опять-таки, нет ни следа от списка Эдит
+        page_test = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Купить павлиньи перья', page_test)
+        self.assertIn('Купить молоко', page_test)
+
+        # Удовлетворенные, они оба ложатся спать
--- superlists/settings.py
+++ superlists/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists.apps.ListsConfig',
+    'functional_tests',
 ]
 
 MIDDLEWARE = [
@@ -55,8 +56,7 @@ ROOT_URLCONF = 'superlists.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
